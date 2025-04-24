6f8cb496e2509e32a4f5254374c7ce1862b929f4
MarkGbla <markgbla16@gmail.com>
2025-04-24 02:38:19 +0000

    Create python week1 Assignment.py

---
+++ python week1 Assignment.py
@@ -0,0 +1,12 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+
+def add(a, b):
+    return a + b
+
+def main(a, b):
+   
+    print("The sum is:", add(a, b))
+
+if __name__ == "__main__":
+    main(a, b)
